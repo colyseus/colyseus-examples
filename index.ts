d26f8f9b4fd364613fe06d2b5872c6bb37b2e9e5
Endel Dreyer <endel.dreyer@gmail.com>
2018-06-06 12:18:04 -0300

    upgrade colyseus versions

--- index.ts
+++ index.ts
@@ -1,26 +1,25 @@
 import * as path from 'path';
 import * as express from 'express';
 import * as serveIndex from 'serve-index';
+import * as WebSocket from 'uws';
 import { createServer } from 'http';
 import { Server } from 'colyseus';
 import { monitor } from '@colyseus/monitor';
 
-// Require ChatRoo handler
+// Import demo room handlers
 import { ChatRoom } from "./rooms/01-chat-room";
 import { StateHandlerRoom } from "./rooms/02-state-handler";
 import { AuthRoom } from "./rooms/03-auth";
 import { CreateOrJoinRoom } from "./rooms/04-create-or-join-room";
 
-const port = Number(process.env.PORT || 2657);
+const port = Number(process.env.PORT || 2567);
 const app = express();
 
 // Attach WebSocket Server on HTTP Server.
 const gameServer = new Server({
-    server: createServer(app)
+  engine: WebSocket.Server,
+  server: createServer(app)
 });
-
-// Register ChatRoom as "chat"
-gameServer.register("chat", ChatRoom);
 
 // Register ChatRoom with initial options, as "chat_with_options"
 // onInit(options) will receive client join options + options registered here.
@@ -42,6 +41,10 @@ app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
 
 // (optional) attach web monitoring panel
 app.use('/colyseus', monitor(gameServer));
+
+gameServer.onShutdown(function(){
+  console.log(`game server is going down.`);
+});
 
 gameServer.listen(port);
 console.log(`Listening on http://localhost:${ port }`);
