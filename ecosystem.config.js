90cf6baeada20975807abd3b9cd050daab9a9fbe
Endel Dreyer <endel.dreyer@gmail.com>
2023-09-05 10:26:55 -0300

    use correct .env files. add ecosystem file for colyseus cloud

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+const os = require('os');
+
+/**
+ * COLYSEUS CLOUD WARNING:
+ * ----------------------
+ * PLEASE DO NOT UPDATE THIS FILE MANUALLY AS IT MAY CAUSE DEPLOYMENT ISSUES
+ */
+
+module.exports = {
+  apps : [{
+    name: "colyseus-app",
+    script: 'build/index.js',
+    time: true,
+    watch: false,
+    instances: os.cpus().length,
+    exec_mode: 'fork',
+    wait_ready: true,
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+};
