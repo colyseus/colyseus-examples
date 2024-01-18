92812345c19232e762558798ff7c5d43f47eab35
Endel Dreyer <endel.dreyer@gmail.com>
2024-01-18 14:20:00 -0300

    add @colyseus/auth module

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,13 @@
 import config from "@colyseus/tools";
 import { monitor } from "@colyseus/monitor";
 import { playground } from "@colyseus/playground";
+import { auth } from "@colyseus/auth";
 import path from 'path';
 import serveIndex from 'serve-index';
 import express from 'express';
 
 // import { uWebSocketsTransport} from "@colyseus/uwebsockets-transport";
+import "./config/auth";
 
 // Import demo room handlers
 import { LobbyRoom, RelayRoom } from 'colyseus';
@@ -61,14 +63,17 @@ export default config({
     },
 
     initializeExpress: (app) => {
-        app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
-        app.use('/', express.static(path.join(__dirname, "static")));
+        // (optional) auth module
+        app.use(auth.prefix, auth.routes());
 
         // (optional) client playground
         app.use('/playground', playground);
 
         // (optional) web monitoring panel
         app.use('/colyseus', monitor());
+
+        app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
+        app.use('/', express.static(path.join(__dirname, "static")));
     },
 
 
